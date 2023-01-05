8b4b3b819e0160a7bde412b0520d073b5182de15
Samuel Patiño Cataño <samuel.campeon@gmail.com>
2023-01-04 20:51:22 -0500

    HomeWorks 1.1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   return parseInt(num, 2)
+   let sum = 0;
+  // tu codigo aca
+   for (let i = 0; i < num.length; i++){
+      sum += +num[i] * 2 ** (num.length - 1 - i);
+   }
+   return sum;
 }
 
 function DecimalABinario(num) {
-   return num.toString(2);
+  // tu codigo aca
+   let binario = (num % 2).toString();
+   for (; num > 1; ) {
+      num = parseInt(num / 2);
+      binario =  (num % 2) + (binario);
+   }
+   return binario;
 }
 
 module.exports = {
