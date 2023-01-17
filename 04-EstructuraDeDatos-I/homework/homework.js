f174e09cab60ed804084285cacf16e893c952034
Samuel Patiño Cataño <samuel.campeon@gmail.com>
2023-01-16 19:45:13 -0500

    Solucion

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,13 +1,43 @@
 'use strict';
 
 // EJERCICIO 1
-function nFactorial(n) {}
+function nFactorial(n) {
+   // caso base
+   if(n === 0 || n === 1) return 1
+   // descontamos negativos
+   else if(n < 0) return 0
+   // aplicar la recurcion 
+   return n * nFactorial(n-1)
+}
 
 // EJERCICIO 2
-function nFibonacci(n) {}
+function nFibonacci(n) {
+   // caso base
+   if(n <= 0) return 0
+   else if(n === 1) return 1
+   return nFibonacci(n-1) + nFibonacci(n-2)
+}
 
 // EJERCICIO 3
-function Queue() {}
+function Queue() {
+   this.array = []
+}
+
+Queue.prototype.enqueue = function(value){
+   return this.array.push(value);
+};
+
+Queue.prototype.dequeue = function (){
+   return this.array.shift()
+}
+
+Queue.prototype.size =function (){
+   return this.array.length
+}
+
+var nuevaQueue = new Queue()
+console.log(nuevaQueue)
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
