6fbcaf4c33193d4423b722166a425cf03dd1ad3d
Samuel Patiño Cataño <samuel.campeon@gmail.com>
2023-02-05 18:55:15 -0500

    terminado 7

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -5,8 +5,23 @@ function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
-  // Tu código:
+  // Tu código:z
 
+
+  var arr = [1]
+  var divisor = 2
+
+while (num !== 1) {
+  if (num % divisor === 0){
+    arr.push(divisor)
+    num = num / divisor
+  }else{
+    divisor++
+  }
+}
+
+
+  return arr
 }
 
 function bubbleSort(array) {
@@ -14,7 +29,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var flag = true
+  while (flag){
+    flag = false
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i+1]){
+        var aux = array[i]
+        array[i] = array[i+1]
+        array[i+1] = aux 
+        flag = true
+      }
+    }
+  }
+  return array
 }
 
 
@@ -23,7 +50,15 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    var currentValue = array[i]
+    var j
+    for (j = i - 1; j >= 0 && array[j] > currentValue; j--) {
+      array[j+1] = array[j]
+    }
+    array[j+1] = currentValue
+  }
+  return array
 }
 
 
@@ -32,7 +67,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j
+      }
+    }
+    if(i !== min){
+      let aux = array[i]
+      array[i] = array[min]
+      array[min] = aux
+    }
+  }
+  return array
 }
 
 
